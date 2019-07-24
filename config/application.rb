758f427e14758dc74f7afa087c788c8ecf019500
Matthieu.D <mattx08@live.fr>
2019-07-24 13:35:09 +0200

    commit

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,17 @@ module SiteRestaurant
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.action_mailer.delivery_method = :smtp
+    # SMTP settings for gmail
+    config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV['gmail_username'],
+    :password             => ENV['gmail_password'],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+    }
+
   end
 end
