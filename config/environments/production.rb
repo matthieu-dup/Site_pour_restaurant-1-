0cf93a991e8c13a61ea9697aeb8098b14770c002
Matthieu.D <mattx08@live.fr>
2019-07-24 13:53:09 +0200

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,6 +94,10 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'site-restaurant-dm-freelance.herokuapp.com' }  
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
